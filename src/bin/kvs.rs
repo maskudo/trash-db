9693648380661c626ee77b5ee2591efe9e30ca31
maskudo <manujkarki101@gmail.com>
2023-09-29 23:52:52 +0545

    feat: impl set, get, rm and serde to json

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,19 +1,26 @@
+use std::process::exit;
+
 use clap::Parser;
 use trash_db::Commands;
-use trash_db::{Cli, KvStore};
+use trash_db::{Cli, KvStore, Result};
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
     let mut kvs = KvStore::new();
     match &cli.command {
-        Commands::Get { key } => {
-            unimplemented!("unimplemented")
-        }
+        Commands::Get { key } => match kvs.get(key.to_owned())? {
+            Some(val) => println!("{val}"),
+            None => {
+                println!("Key not found");
+                exit(0)
+            }
+        },
         Commands::Set { key, value } => {
-            unimplemented!("unimplemented")
+            kvs.set(key.clone(), value.clone())?;
         }
         Commands::Rm { key } => {
-            unimplemented!("unimplemented")
+            kvs.remove(key.clone())?;
         }
     }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use std::{
     collections::HashMap,
     error::Error,
+    fs::{File, OpenOptions},
+    io::{BufReader, BufWriter, Write},
     path::{Path, PathBuf},
 };
 
 use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
 pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Parser)]
@@ -32,34 +35,90 @@ pub enum Commands {
     },
 }
 
+// # TODO : Compaction
+#[derive(Serialize, Deserialize)]
 pub struct KvStore {
     store: HashMap<String, String>,
+    #[serde(skip_serializing)]
     path: PathBuf,
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl KvStore {
     pub fn new() -> Self {
+        let pathbuf = PathBuf::from("./store");
+        let file = File::open(&pathbuf);
+        let content = match file {
+            Ok(file) => {
+                let file_reader = BufReader::new(file);
+                let content: HashMap<String, String> = match serde_json::from_reader(file_reader) {
+                    Ok(content) => content,
+                    Err(_) => HashMap::default(),
+                };
+
+                content
+            }
+            Err(_) => HashMap::default(),
+        };
+
         KvStore {
-            store: HashMap::default(),
-            path: PathBuf::from("./store.log"),
+            store: content,
+            path: pathbuf,
         }
     }
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&self.path)?;
+        let mut writer = BufWriter::new(file);
         self.store.insert(key, value);
+        let content = serde_json::to_string(&self.store)?;
+        write!(writer, "{}", content)?;
         Ok(())
     }
     pub fn get(&self, key: String) -> Result<Option<String>> {
         Ok(self.store.get(&key).cloned())
     }
     pub fn remove(&mut self, key: String) -> Result<()> {
-        self.store
-            .remove(&key)
-            .map_or_else(|| Err(From::from("Error removing key")), |_| Ok(()))
+        let res = self.store.remove(&key);
+        match res {
+            Some(_) => {}
+            None => {
+                println!("Key not found");
+                return Err(From::from(""));
+            }
+        }
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&self.path)?;
+        let mut writer = BufWriter::new(file);
+        let content = serde_json::to_string(&self.store)?;
+        write!(writer, "{}", content)?;
+        Ok(())
     }
     pub fn open(path: &Path) -> Result<Self> {
+        let mut pathbuf = PathBuf::from(path);
+        pathbuf.push("store");
+        let file = File::open(&pathbuf);
+        let content = match file {
+            Ok(file) => {
+                let file_reader = BufReader::new(file);
+                let content: HashMap<String, String> = serde_json::from_reader(file_reader)?;
+                content
+            }
+            Err(_) => HashMap::default(),
+        };
+
         Ok(KvStore {
-            store: HashMap::default(),
-            path: PathBuf::from(path),
+            store: content,
+            path: pathbuf,
         })
     }
 }
